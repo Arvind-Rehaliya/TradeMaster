2a970e9f9350ff6ccf836b000c664e4443169097
arvind <arvind@no-mail.com>
2020-02-13 23:27:22 +0530

    Add project files.

---
+++ TradeMaster/wpf/AddDetailsDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace TradeMaster.wpf
+{
+    /// <summary>
+    /// Interaction logic for AddDetailsDialog.xaml
+    /// </summary>
+    public partial class AddDetailsDialog : Window
+    {
+        public AddDetailsDialog()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Close_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+            this.Close();
+        }
+
+        private void Submit_Clicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submit Clicked");
+
+        }
+
+    }
+}
